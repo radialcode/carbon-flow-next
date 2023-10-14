d9b25585042caeaf638643e0eaa5debd42d3e8c0
pk-jhajhria <jhajhriapankaj13@gmail.com>
2023-10-14 15:11:38 +0530

    slider arrow pensing

--- src/components/homepage/TeamMember.jsx
+++ src/components/homepage/TeamMember.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Slider from "react-slick";
 import { TeamMemberSliderdata } from "../common/Helper";
+import Image from "next/image";
+import { TeamSliderArrowIcon } from "../common/Icon";
 
 const TeamMember = () => {
   var settings = {
-    dots: true,
+    dots: false,
     infinite: true,
     speed: 500,
     slidesToShow: 4,
@@ -14,7 +16,7 @@ const TeamMember = () => {
       {
         breakpoint: 1200,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 3,
           slidesToScroll: 1,
           autoplay: true,
           autoplaySpeed: 2000,
@@ -24,7 +26,7 @@ const TeamMember = () => {
       {
         breakpoint: 1008,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 3,
           slidesToScroll: 1,
           autoplay: true,
           autoplaySpeed: 2000,
@@ -55,28 +57,49 @@ const TeamMember = () => {
             by a shared commitment to global standards of excellence.
           </p>
 
-          <Slider {...settings} className="flex flex-wrap mx-[-12px]">
-            {TeamMemberSliderdata.map((data, index) => {
-              return (
-                <div className="w-3/12 h-full flex" key={index}>
-                  <div className="border-2 border-[#E6E9E7] hover:border-[#D2F958] rounded-[10px] h-full w-full">
-                    <div className="px-[76px] md:px-[79px] py-[22px]">
-                      <span>{data.svgicon}</span>
-                      <h2 className="ff_AktivGrotesk_bold font-normal text-[20px] text-[#061E10] text-center mb-0 mt-[24px] leading-[24.04px]">
-                        {data.heading}
-                      </h2>
-                      <p className=" ff_poppins font-normal text-[14px] text-[#061E10] mb-0 mt-[2px] text-center">
-                        {data.profession}
-                      </p>
-                      <button className="mt-[16px] ff_poppins font-medium text-[14px] text-white py-[12.5px] px-[22px] border-2 border-white rounded-full bg-[#235C02] hover:bg-white hover:text-[#4EB708] hover:border-[#4EB708] duration-200">
-                        {data.profileBtn}
-                      </button>
+          <div className="relative">
+            <div className="top-[50%] translate-y-[-50%] left-[-6%] absolute group cursor-pointer">
+              <TeamSliderArrowIcon />
+            </div>
+            <div className="top-[50%] translate-y-[-50%] right-[-6%] absolute rotate-[180deg] group cursor-pointer">
+              <TeamSliderArrowIcon />
+            </div>
+            <Slider
+              {...settings}
+              className="flex flex-wrap mt-[40px] mx-[-10px] relative"
+            >
+              {TeamMemberSliderdata.map((data, index) => {
+                return (
+                  <div className="w-3/12 h-full flex px-[10px]" key={index}>
+                    <div className="border-2 border-[#E6E9E7] hover:border-[#D2F958] rounded-[10px] h-full w-full">
+                      <div className="py-[22px]">
+                        <div className="clip_path_team_slider flex justify-center overflow-hidden">
+                          <Image
+                            className="w-full scale-110 px-[66px]"
+                            width={96}
+                            height={110}
+                            src={data.img}
+                            alt="team slider image 1"
+                          />
+                        </div>
+                        <h2 className="ff_AktivGrotesk_bold font-normal text-[20px] text-[#061E10] text-center mb-0 mt-[12px] leading-[24.04px]">
+                          {data.heading}
+                        </h2>
+                        <p className=" ff_poppins font-normal text-[14px] text-[#061E10] mb-0 mt-[2px] text-center">
+                          {data.profession}
+                        </p>
+                        <div className="flex justify-center">
+                          <button className="mt-[16px] ff_poppins font-medium text-[14px] text-white py-[12.5px] px-[22px] border-2 border-white rounded-full bg-[#235C02] hover:bg-white hover:text-[#4EB708] hover:border-[#4EB708] duration-200">
+                            {data.profileBtn}
+                          </button>
+                        </div>
+                      </div>
                     </div>
                   </div>
-                </div>
-              );
-            })}
-          </Slider>
+                );
+              })}
+            </Slider>
+          </div>
         </div>
       </section>
     </>
