c84bdc936d415fd6e7b1cbe22fd9d5053603e28a
💻ISHU DEVELOPER💻 <ishudeveloper@gmail.com>
2023-10-16 22:15:51 +0530

    add arrows in slider

--- src/components/homepage/TeamMember.jsx
+++ src/components/homepage/TeamMember.jsx
@@ -83,15 +83,15 @@ const TeamMember = () => {
           <div className="relative">
             <div
               onClick={() => first.current.slickPrev()}
-              className="top-[50%] translate-y-[-50%] -left-[5%] 2xl:left-[-6%] absolute group cursor-pointer hidden xl:block"
+              className="top-[50%] translate-y-[-50%] -left-[5%] 2xl:left-[-6%] absolute group arrows cursor-pointer hidden xl:block border-2 border-[#44B902] border-solid  p-3 rounded-full"
             >
               <TeamSliderArrowIcon />
             </div>
             <div
               onClick={() => first.current.slickNext()}
-              className="top-[50%] translate-y-[-50%] -right-[5%] 2xl:right-[-6%] absolute rotate-[180deg] group cursor-pointer hidden xl:block"
+              className="top-[50%] translate-y-[-50%] -right-[5%] 2xl:right-[-6%] absolute rotate-[180deg] group arrows cursor-pointer hidden xl:block border-2 border-[#44B902] border-solid  p-3 rounded-full"
             >
-              <TeamSliderArrowIcon />
+              <TeamSliderArrowIcon className="fill-white" />
             </div>
             <Slider
               {...settings}
