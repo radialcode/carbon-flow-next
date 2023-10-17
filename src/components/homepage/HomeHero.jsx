a6ab00f1e4ca77a08f19957593f17c3843ac5214
olla_rakesh <rakesh10448olla@gmail.com>
2023-10-17 15:15:33 +0530

    code merge

--- src/components/homepage/HomeHero.jsx
+++ src/components/homepage/HomeHero.jsx
@@ -5,9 +5,6 @@ import "intersection-observer";
 
 import { ScrollDownIcon } from "../common/Icon";
 const HomeHero = () => {
-  const [ref, inView] = useInView({
-    triggerOnce: true,
-  });
 
   return (
     <>
@@ -50,10 +47,7 @@ const HomeHero = () => {
             <div className="w-full xs:w-[75%] lg:w-1/2">
               <div className="hero_clipPath lg:absolute right-1/2 mt-[88px] 2xsm:mt-[80px] xsm:mt-[90px] sm:mt-24 md:mt-[80px] bottom-[-60px] xl:bottom-[-120px] lg:right-[-60px] xl:right-[-80px] 2xl:right-[-88px]">
                 <Image
-                  ref={ref}
-                  className={`object-cover ${
-                    inView ? "hero_img_scale " : "hero_img_scale"
-                  }  hero_clipPath w-full lg:max-w-[550px] xl:max-w-[700px] 2xl:max-w-[750px] 3xl:max-w-[900px] xl:h-[700px] 2xl:h-[750px] 3xl:h-[900px] h-[350px] xsm:h-[600px] sm:h-[550px] `}
+                  className="object-cover hero_img_scale hero_clipPath w-full lg:max-w-[550px] xl:max-w-[700px] 2xl:max-w-[750px] 3xl:max-w-[900px] xl:h-[700px] 2xl:h-[750px] 3xl:h-[900px] h-[350px] xsm:h-[600px] sm:h-[550px] "
                   src="/images/webp/forrest_hero_img.webp"
                   width={808}
                   height={846}
