a1796469a356d56a6fbcd9176d667d14e4941c99
Sanjay2929 <sanjayjyani220@gmail.com>
2023-10-23 12:16:04 +0530

    loader update

--- src/components/homepage/HomeHero.jsx
+++ src/components/homepage/HomeHero.jsx
@@ -11,18 +11,22 @@ const HomeHero = ({loader}) => {
   });
   return (
     <>
-      <div className={`bg-[#061E10]  flex flex-col overflow-hidden relative pt-[50px] lg:pt-0 -mt-[1px] ${loader ? "h-screen" : "hero_screen"}`}>
-      <a
-        className="absolute ff_poppins font-light group text-[14px] text-white hidden -rotate-90 bottom-[140px] left-[-56px] uppercase tracking-[11px] xl:flex items-center gap-[17px] z-10"
-        href="#pitchingdeck"
+      <div
+        className={`bg-[#061E10]  flex flex-col overflow-hidden relative pt-[50px] lg:pt-0 -mt-[1px] ${
+          loader ? "h-screen" : "hero_screen"
+        }`}
       >
-        <span className="scrolldown_animation rotate-90 w-5 h-5 flex flex-col overflow-hidden duration-200 relative">
-          <span className="group-hover:top-0 absolute -top-5 left-0 duration-200">
-            <ScrollDownIcon /> <ScrollDownIcon />
+        <a
+          className="absolute ff_poppins font-light group text-[14px] text-white hidden -rotate-90 bottom-[140px] left-[-56px] uppercase tracking-[11px] xl:flex items-center gap-[17px] z-10"
+          href="#pitchingdeck"
+        >
+          <span className="scrolldown_animation rotate-90 w-5 h-5 flex flex-col overflow-hidden duration-200 relative">
+            <span className="group-hover:top-0 absolute -top-5 left-0 duration-200">
+              <ScrollDownIcon /> <ScrollDownIcon />
+            </span>
           </span>
-        </span>
-        Scroll Down
-      </a>
+          Scroll Down
+        </a>
         {/* <div className=" container xl:max-w-[1140px] 3xl:max-w-[1320px] px-3 xl:px-0 flex flex-grow mx-auto">
           <div className=" flex flex-col lg:flex-row items-center lg:justify-between ">
             <div className="w-full relative">
@@ -73,12 +77,17 @@ const HomeHero = ({loader}) => {
         </div> */}
         <div className="custom_container w-full h-full mx-auto">
           <div className="flex flex-col items-center lg:flex-row justify-between h-full">
-          <div className="w-full relative" data-aos="fade-right" data-aos-delay="800" data-aos-duration="900">
+            <div
+              className="w-full relative"
+              data-aos="fade-right"
+              data-aos-delay="800"
+              data-aos-duration="900"
+            >
               <h1 className="ff_AktivGrotesk_bold text-[36px] sm:text-[40px] md:text-[55px] xl:text-[70px] font-normal leading-[115%] capitalize text-white">
                 Direct <span className="text-[#44B902]">Measure,</span>
                 <span className=" block">
                   Direct <span className="text-[#44B902]">Matter !</span>
-                </span>   
+                </span>
               </h1>
               <p className="ff_poppins text-base font-normal capitalize leading-[160%] text-white pt-[18px] sm:w-[542px] w-full">
                 The Most Effective way to Measure Forrest & Farm's Carbon
@@ -87,37 +96,56 @@ const HomeHero = ({loader}) => {
                 credit.
               </p>
               <div className="lg:pt-[35px] pt-[20px]">
-            <span className="inline-block">
-            <a href="mailto:contactus@carbonflow.earth" className="px-[24px] py-[12px] border group border-[#44B902] ff_poppins font-semibold text-[16px] text-[#FFFFFF] rounded-full hover:bg-[#44B902] duration-200 hover:text-white leading-6 flex gap-[23px] items-center">
-              Start Measure!
-              <span className="ff_poppins font-semibold text-[10px] text-[#FFFFFF] block leading-[18px] group-hover:text-white opacity-50 after:contents-[''] after:absolute relative after:h-[18px] after:w-[1px] after:bg-[#FFFFFF] after:-left-3 after:top-1/2 after:-translate-y-1/2">
-                No CC Required
-              </span>
-            </a>
-            </span>
-          </div>
-                <p className="text-[12px] ff_poppins font-extralight text-white sm:max-w-[310px] max-w-[420px] mt-3 sm:mt-9">
-              Based on 5 years research published in
-                </p>
+                <span className="inline-block">
+                  <a
+                    href="mailto:contactus@carbonflow.earth"
+                    className="px-[24px] py-[12px] border group border-[#44B902] ff_poppins font-semibold text-[16px] text-[#FFFFFF] rounded-full hover:bg-[#44B902] duration-200 hover:text-white leading-6 flex gap-[23px] items-center"
+                  >
+                    Start Measure!
+                    <span className="ff_poppins font-semibold text-[10px] text-[#FFFFFF] block leading-[18px] group-hover:text-white opacity-50 after:contents-[''] after:absolute relative after:h-[18px] after:w-[1px] after:bg-[#FFFFFF] after:-left-3 after:top-1/2 after:-translate-y-1/2">
+                      No CC Required
+                    </span>
+                  </a>
+                </span>
+              </div>
+              <p className="text-[12px] ff_poppins font-extralight text-white sm:max-w-[310px] max-w-[420px] mt-3 sm:mt-9">
+                Based on 5 years research published in
+              </p>
               <Link href="/">
-              <Image className="mt-3 max-w-[250px]" height={100} width={200} src="/images/JGR_logo.svg" alt="JGR_logo"/>
+                <Image
+                  className="mt-3 max-w-[250px]"
+                  height={100}
+                  width={200}
+                  src="/images/JGR_logo.svg"
+                  alt="JGR_logo"
+                />
               </Link>
             </div>
-            <div className={`${loader ? "loader_border z-[999] absolute top-[50%] right-[50%] translate-x-[38%] -translate-y-[50%] duration-[2s] max-w-[100px] h-[100px]" : "absolute hero_clipPath md:top-[57%] top-[70.5%] right-[50%] translate-x-[50%] lg:top-[58%] xl:top-[33%] 3xl:top-[43%] md:-right-[55px] lg:-right-[55px] xl:-right-[80px] md:translate-x-0 -translate-y-[18%] duration-[2s] animate_loader md:w-[480px] lg:w-auto md:max-w-[600px] xl:max-w-[700px] md:h-[600px] xl:h-[700px] 4xl:max-w-[1200px]  4xl:h-[1200px]"}`}>
-                <Image
-                  ref={ref}
-                  priority={true}
-                  className={`object-cover md:w-full md:h-full ${inView ? "hero_img_scale" : ""} ${
-                    loader ? "sm:w-full w-[400px] h-[400px]" : "max-w-[330px] md:max-w-none h-[390px]"
-                  }  hero_clipPath`}
-                  src={`${loader ? "/images/webp/faq-bg-img.webp" : "/images/webp/forrest_hero_img.webp"}`}
-                  width={808}
-                  height={846}
-                  alt="blockchain"
-                />
-              </div>
+            <div
+              className={`${
+                loader
+                  ? "loader_border z-[999] absolute top-[50%] right-[50%] translate-x-[38%] -translate-y-[50%] duration-[2s] max-w-[100px] h-[100px] p-[1] hero_clipPath"
+                  : "absolute hero_clipPath md:top-[57%] top-[70.5%] right-[50%] translate-x-[50%] lg:top-[58%] xl:top-[33%] 3xl:top-[43%] md:-right-[55px] lg:-right-[55px] xl:-right-[80px] md:translate-x-0 -translate-y-[18%] duration-[2s] animate_loader md:w-[480px] lg:w-auto md:max-w-[600px] xl:max-w-[700px] md:h-[600px] xl:h-[700px] 4xl:max-w-[1200px]  4xl:h-[1200px]"
+              }`}
+            >
+              <Image
+                ref={ref}
+                priority={true}
+                className={`object-cover md:w-full md:h-full ${
+                  inView ? "hero_img_scale" : ""
+                } ${
+                  loader
+                    ? "sm:w-full w-[400px] h-[400px] loader_animation rounded-full bg-[#44B902]"
+                    : "max-w-[330px] md:max-w-none h-[390px] bg-[#235C02]"
+                } `}
+                src={`${loader ? "" : "/images/png/satelite.png"}`}
+                width={808}
+                height={846}
+                alt="blockchain"
+              />
             </div>
           </div>
+        </div>
       </div>
     </>
   );
